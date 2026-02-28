3a3279e4c01b42f7519c7731c652bdbd2f28eba4
GitHub Action <action@github.com>
2026-02-28 02:01:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/woailv/inplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/woailv/inplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.woailv.inplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.woailv.inplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
